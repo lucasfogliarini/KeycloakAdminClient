459d9d50bcdfa6aac7773643f39b5835fce93c40
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-09-17 10:33:48 -0300

    Add project files.

---
+++ KeycloakAdminClient/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Duende.AccessTokenManagement;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KeycloakAdminClient;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddKeycloakAdminClient(this IServiceCollection services)
+    {
+        var keycloakAdminClient = ClientCredentialsClientName.Parse("keycloak-admin.client");
+        services.AddClientCredentialsTokenManagement()
+              .AddClient(keycloakAdminClient, client =>
+              {
+                  client.TokenEndpoint = new Uri("http://localhost:1100/realms/bora/protocol/openid-connect/token");
+                  client.ClientId = ClientId.Parse("bora-client");
+                  client.ClientSecret = ClientSecret.Parse("AQXrW4Inbt8RyuMHxASqXvNNmZGTAn7V");
+                  client.Scope = Scope.Parse("managed-users");
+              });
+
+
+        services.AddClientCredentialsHttpClient("users",
+          keycloakAdminClient,
+          client =>
+          {
+              client.BaseAddress = new Uri("http://localhost:1100/admin/realms/bora/users");
+          });
+
+        //services.AddHttpClient<KeycloakUsersClient>(client =>
+        //{
+        //    client.BaseAddress = new Uri("http://localhost:1100/admin/realms/bora/users");
+        //})
+        //.AddClientCredentialsTokenHandler(keycloakAdminClient);
+
+        return services;
+    }
+}
---
+++ KeycloakAdminClient/KeycloakUsersClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace KeycloakAdminClient;
+
+public class KeycloakUsersClient(HttpClient httpClient)
+{
+    public async Task<IEnumerable<KeycloakUser>> GetUsersAsync(CancellationToken cancellationToken = default)
+    {
+        var response = await httpClient.GetAsync("users", cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        var users = await response.Content.ReadFromJsonAsync<IEnumerable<KeycloakUser>>(
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            },
+            cancellationToken
+        );
+
+        return users ?? Enumerable.Empty<KeycloakUser>();
+    }
+}
+
+public class KeycloakUser
+{
+    public string Id { get; set; } = default!;
+    public string Username { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public bool Enabled { get; set; }
+    public IEnumerable<string>? RealmRoles { get; set; }
+}
